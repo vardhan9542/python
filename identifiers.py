b9da4fb9f0121bd620b01ad152fd07b7dce643cb
vardhan9542 <guna18vardhan@gmail.com>
2026-07-27 12:59:25 +0530

    Add files via upload

---
+++ identifiers.py
@@ -0,0 +1,22 @@
+number=10
+name="vardhan"
+student_id=502
+max_value=100
+college_name="gmr"
+
+print("number=",number)
+print("name=",name)
+print("student_id=",student_id)
+print("max_value=",max_value)
+print("college_name=",college_name)
+
+
+#number= 10
+#name= vardhan
+#student_id= 502
+#max_value= 100
+#college_name= gmr
+
+
+
+
---
+++ marks.py
@@ -0,0 +1,8 @@
+Marks=95
+marks=80
+print("Marks=",Marks)
+print("marks=",marks)
+
+
+#Marks= 95
+#marks= 80
