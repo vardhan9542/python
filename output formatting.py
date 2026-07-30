1b122eec55a585362c5e118378f649314011b235
vardhan9542 <guna18vardhan@gmail.com>
2026-07-30 17:02:17 +0530

    Update name and output formatting in script

--- output formatting.py
+++ output formatting.py
@@ -1,12 +1,12 @@
-#Y.Mukhesh
+#N.Guna vardhan
 #output formatting methods
-name = "Mukhesh"
+name = "vardhan"
 marks = 96
 print("Name:", name, "Marks:", marks)
 print("Name: {} Marks: {}".format(name, marks))
 print(f"Name: {name} Marks: {marks}")
 
 #output
-#Name: Mukhesh Marks: 96
-#Name: Mukhesh Marks: 96
-#Name: Mukhesh Marks: 96
+#Name: vardhan Marks: 96
+#Name: vardhan Marks: 96
+#Name: vardhan Marks: 96
