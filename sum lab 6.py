cb83dfa866cc82e9400a7b52955c3f619b377b98
vardhan9542 <guna18vardhan@gmail.com>
2026-07-30 22:05:12 +0530

    Delete sum lab 6.py

--- sum lab 6.py
+++
@@ -1,9 +0,0 @@
-#N.Guna vardhan
-#seperated by spaces
-numbers = input("Enter numbers separated by spaces: ").split()
-numbers = list(map(int, numbers))
-print("Sum =", sum(numbers))
-
-#output
-#Enter numbers separated by spaces: 10 20 30 40 50
-#Sum = 150
