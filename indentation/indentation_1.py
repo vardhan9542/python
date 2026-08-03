7405563784c13d4c46f3a73775882ec10e5345cb
vardhan9542 <guna18vardhan@gmail.com>
2026-08-03 11:57:58 +0530

    Add files via upload

---
+++ indentation/indentation_1.py
@@ -0,0 +1,8 @@
+# Program with an indentation error
+
+num = 10
+
+if num > 0:
+    print("Positive Number")
+else:
+    print("Negative Number")
---
+++ indentation/indentation_2.py
@@ -0,0 +1,8 @@
+# Outer for loop
+for i in range(1, 11):
+
+    # Inner if-else statement
+    if i % 2 == 0:
+        print(i, "- Even")
+    else:
+        print(i, "- Odd")
