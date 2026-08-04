6ae1bc41a32c6ce2d00b960bf0a55f8e6f88b283
vardhan9542 <guna18vardhan@gmail.com>
2026-08-04 20:08:06 +0530

    Delete input output statements directory

--- input output statements/input_output_statements_1.py
+++
@@ -1,8 +0,0 @@
-# Take the user's name as input
-name = input("Enter your name: ")
-
-# Take the user's age as input
-age = int(input("Enter your age: "))
-
-# Print the required message
-print("Hello", name + ", you will turn", age + 1, "next year.")
